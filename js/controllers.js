d63297a2da2c0eb998c95075d01ef947e11c54a5
Fabio Rocha <fabio@fabiofarias.com.br>
2014-04-16 20:15:33 -0300

    Inner ok

--- js/controllers.js
+++ js/controllers.js
@@ -111,9 +111,67 @@ meusControllers.controller('addLancController', function($scope, $location, $rou
 });
 meusControllers.controller('lancamentoController',
         function($scope, $location, $routeParams, CategoriaService, PgtoService, LancamentoService) {
-            LancamentoService.getAll(function(data) {
-                console.log(data.results);
-            });
+            /*LancamentoService.getAll(function(data) {
+                angular.forEach(data.results, function(value, key) {
+                  //  console.log(value.categoria);
+                    
+                });
+            });*/
+        });
+        
+        
+        
+         Parse.initialize("ezwTgQirFdjt1dnPiidr0nV1eqr9ARiOa3h43CgL", "S64qIhW7OhynJErsvmmYG1dV2nKw2YUk42AckFKK");
+        /*
+        // Declare the types.
+var Post = Parse.Object.extend("Post");
+var Comment = Parse.Object.extend("Comment");
+ 
+// Create the post
+var myPost = new Post();
+myPost.set("title", "I'm Hungry");
+myPost.set("content", "Where should we go for lunch?");
+ 
+// Create the comment
+var myComment = new Comment();
+myComment.set("content", "Let's do Sushirrito.");
+ 
+// Add the post as a value in the comment
+myComment.set("parent", myPost);
+ 
+// This will save both myPost and myComment
+myComment.save();*/
+
+var Comment = Parse.Object.extend("Comment");
+
+var query = new Parse.Query(Comment);
+ 
+// Retrieve the most recent ones
+query.descending("createdAt");
+ 
+// Only retrieve the last ten
+query.limit(1);
+ 
+// Include the post data with each comment
+query.include("Post");
+//query.include(["post.author"]);
+
+
+query.find({
+  success: function(comments) {
+     // console.log(comments.parent);
+     
+    // Comments now contains the last ten comments, and the "post" field
+    // has been populated. For example:
+    for (var i = 0; i < comments.length; i++) {
+        console.log(comments[i].get('content'));
+        var Post = comments[i].get('parent').fetch();
+        
+        console.log(Post.get('content'));
+      // This does not require a network access.
+      //var post = comments[i].get("post.nome");
+      
+     // console.log(post);
+    }
+  }
 });
-
-
